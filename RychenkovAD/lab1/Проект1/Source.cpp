a3a9525e3d5d81f2419ca85117e78ef9b066b6ae
MercyFlesh <con2288@mail.ru>
2018-09-25 00:11:27 +0300

    [READY}RychenkovAD_lab1
    like this?

--- RychenkovAD/lab1/Проект1/Source.cpp
+++ RychenkovAD/lab1/Проект1/Source.cpp
@@ -5,41 +5,39 @@ using namespace std;
 
 int main()
 {
-	float first, second, resault;
-	bool flag = true;
-	char sign;
-	setlocale(0, "rus");
-	cout << "¬вести первое число: ";
-	cin >> first;
-	cout << "¬вести второе число: ";
-	cin >> second;
-	cout << "¬вести операцию: ";
-	cin >> sign;
+		float first, second, resault;
+		bool flag = true;
+		char sign;
+		setlocale(0, "rus");
+		cout << "¬вести первое число: ";
+		cin >> first;
+		cout << "¬вести второе число: ";
+		cin >> second;
+		cout << "¬вести операцию: ";
+		cin >> sign;
 	
-	    switch (sign) {
-		case '+': 
-			resault = first + second; break;
-		case '-':
-			resault = first - second; break;
-		case '*': 
-			resault = first * second; break;
-		case '/':
-			if (second == 0) {
-				flag = false;
-				cout << "ќшибка ввода!" << endl; break;
+		switch(sign){
+			case '+': 
+				resault = first + second; break;
+			case '-':
+				resault = first - second; break;
+			case '*': 
+				resault = first * second; break;
+			case '/':
+				if (second == 0) {
+					flag = false;
+					cout << "ќшибка ввода!" << endl; break;
+				}
+				else {
+					resault = first / second; break;
+				}
+			default:{
+			       flag = false;
+			       cout << "ќшибка ввода!" << endl;
 			}
-			else {
-				resault = first / second; break;
-			}
-		default:
-		   {
-	       flag = false;
-		   cout << "ќшибка ввода!" << endl;
 		}
-	}
-	if (flag) {
-	cout << first << " " << sign << " " << second << " " << "=" << " " << resault << endl;
-    }
+	    if(flag){
+		       cout << first << " " << sign << " " << second << " " << "=" << " " << resault << endl;
+	    }
 
-	
 }
